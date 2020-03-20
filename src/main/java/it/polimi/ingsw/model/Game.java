54297d15b522abb402d168f4435751acfa13dfc9
andreasuperchi <10580672@polimi.it>
2020-03-20 16:20:59 +0100

    Creazione delle prime due classi Game e Player

---
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.model;
+
+public class Game {
+
+    private int numberOfPLayers;
+    private Player currentPlayer;
+    private boolean gameOver;
+
+}
---
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.model;
+
+public class Player {
+
+    private String name;
+    private int age;
+    private String color;
+    private boolean isInGame;
+    private String god;
+
+    public void chooseGod(String godName) {
+
+    }
+}
